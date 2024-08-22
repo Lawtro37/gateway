9cb7dda3d6dc6205ac4cce3c06f60664950cce15
Lawtro37 <98205608+Lawtro37@users.noreply.github.com>
2024-08-22 18:52:41 +1000

    Update server.js

--- gateway/server.js
+++ gateway/server.js
@@ -59,7 +59,7 @@ function getNetworkIP() {
 const networkIP = "https://gateway-2g0m.onrender.com";
 console.log(`Server IP address: ${networkIP} (${getNetworkIP()})`);
 
-const server = https.createServer(async (req, res) => {
+const server = http.createServer(async (req, res) => {
     let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     if (req.url === '/favicon.ico') {
         res.writeHead(200, { 'Content-Type': 'image/x-icon' });
