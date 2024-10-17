d6ff357afdc0b586c95242dc903742fa8860ca5f
Lawtro37 <98205608+Lawtro37@users.noreply.github.com>
2024-10-17 19:03:12 +1000

    Update server.js

--- server.js
+++ server.js
@@ -742,10 +742,11 @@ const server = http.createServer(async (req, res) => {
 
         const ipAddress = dnsResponse.answers[0].data;
 
+        //log the ip address
         log("DoH ip: " + ipAddress)
         log("obfuscated url: https://" + ipAddress + new URL(requestedSite).href)
         log("sanitised url: https://" + encodeURI(ipAddress + new URL(requestedSite).href))
-        
+
         // Fetch the HTML content from the target URL with headers and timeout
         let response = await axios.get("https://" + encodeURI(ipAddress + new URL(requestedSite).href), {
             headers: {
